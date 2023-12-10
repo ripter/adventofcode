21819ad7f3d5a27d4af617326d6b567026db718e
Chris <ripter001@gmail.com>
2023-12-10 15:12:38 -0500

    WIP: Adding timming data

--- 2023/day5/main.nim
+++ 2023/day5/main.nim
@@ -2,10 +2,12 @@ import std/os
 import std/re
 import std/sequtils
 import std/strutils
-
-import nimprof
+import std/times
 
 import ../day2/fileutils
+
+when compileOption("profiler"):
+  import nimprof
 
 const USE_TEST_DATA = true
 const filePath = if USE_TEST_DATA: "./test.txt" else: "./input.txt"
@@ -36,6 +38,7 @@ type
 #
 # Load from the file
 proc initAlmanac(lines: seq[string]): Almanac =
+  let startTime = cpuTime()
   ## Converts the lines from the file into an Almanac
   var output: Almanac
   var entry: AlmanacEntry = (label: "", ranges: @[])
@@ -62,21 +65,29 @@ proc initAlmanac(lines: seq[string]): Almanac =
   if entry.label != "":
     output.add(entry)
 
+  let endTime = cpuTime()
+  echo "initAlmanac() - Time taken: ", endTime - startTime, " seconds"
   return output
 
 
 #
 # Returns true when num is inside
 proc isInRange(num: int, range: AlmanacRange): bool =
+  let startTime = cpuTime()
   let min = range.src
   let max = range.src + (range.length)
   if (num >= min) and (num < max):
+    let endTime = cpuTime()
+    echo "isInRange() - Time taken: ", endTime - startTime, " seconds"
     return true
 
+  let endTime = cpuTime()
+  echo "isInRange() - Time taken: ", endTime - startTime, " seconds"
 
 #
 # Converts num to a mapped id based on entry
 proc toMappedId(num: int, entry: AlmanacEntry): int =
+  let startTime = cpuTime()
   var rangeIdx: int = -1
 
   # Find the first range than contains num
@@ -88,33 +99,44 @@ proc toMappedId(num: int, entry: AlmanacEntry): int =
   let isInNumInRange = rangeIdx != -1
 
   if not isInNumInRange:
+    let endTime = cpuTime()
+    echo "calcLastId() - Time taken: ", endTime - startTime, " seconds"
     # Not in any range. num maps to num.
     return num
 
   # num is in range, convert the value
   let mapRange = entry.ranges[rangeIdx]
   let offset = num - mapRange.src
+
+  let endTime = cpuTime()
+  echo "calcLastId() - Time taken: ", endTime - startTime, " seconds"
   return mapRange.dest + offset
 
 
 
 proc calcLastId(seedId: int, almanac: Almanac): int =
+  let startTime = cpuTime()
   ## Calculates the final ID by iterating through each AlmanacEntry in the provided Almanac.
   ## In each iteration, the current ID is transformed based on the AlmanacEntry using `toMappedId`.
   var resultId: int = seedId
   for entry in almanac:
     resultId = resultId.toMappedId(entry)
-  resultId
+
+  let endTime = cpuTime()
+  echo "calcLastId() - Time taken: ", endTime - startTime, " seconds"
+  return resultId
 
 #
 # Main
 # 
+let appStartTime = cpuTime()
 let rawTextLines: seq[string] = readFileLines(filePath)
 let startingSeedIds = rawTextLines[0].findAll(patternNumbers).map(parseInt)
 let almanac = initAlmanac(rawTextLines[1..^1])
 
 echo "\n--- Part One ---\n"
 echo "startingSeedIds ", startingSeedIds
+let partOneStartTime = cpuTime()
 # echo "almanac ", almanac
 echo "-"
 
@@ -126,13 +148,19 @@ for seedId in startingSeedIds:
 echo "results: ", results
 let partOneValue = results.min
 echo "Answer ", partOneValue 
+
 if USE_TEST_DATA and partOneValue == 35:
   echo "Success!"
+elif partOneValue == 196167384:
+  echo "Success!"
 
+let partOneEndTime = cpuTime()
+echo "Part One - Time taken: ", partOneEndTime - partOneStartTime, " seconds"
 
 
 
 echo "\n--- Part Two ---\n"
+let partTwoStartTime = cpuTime()
 var seedGroups: seq[SeedRange] = startingSeedIds.distribute(2).mapIt((it[0], it[1]))
 echo "seedGroups ", seedGroups
 
@@ -156,3 +184,11 @@ if USE_TEST_DATA and partTwoValue == 46:
 else:
   if partTwoValue == 702443113:
     echo "Oops, Wrong Answer. Too high."
+  else:
+    echo "New Answer! Try it!"
+
+let partTwoEndTime = cpuTime()
+echo "Part Two - Time taken: ", partTwoEndTime - partTwoStartTime, " seconds"
+
+let appEndTime = cpuTime()
+echo "Total Time taken: ", appEndTime - appStartTime, " seconds"
