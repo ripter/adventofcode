df03ca1f102999c8e206d325b162cdd2a3226ef4
Chris <ripter001@gmail.com>
2023-12-16 00:12:11 -0500

    WIP: day 8 Part 1

--- 2023/day8/main.nim
+++ 2023/day8/main.nim
@@ -1,6 +1,8 @@
 import std/os
 import std/strformat
+import std/re
 import times
+import tables
 
 import ../day2/fileutils
 import ../day5/formatutils
@@ -23,6 +25,26 @@ if not fileExists(filePath):
 # ZZZ = ZZZ, ZZZ, ZZZ
 #
 
+type
+  NavigationMap = string
+  NodeID = string
+  NodePair = tuple[left: NodeID, right: NodeID]
+  NodeMap = TableRef[NodeID, NodePair]
+
+proc get(map: NodeMap, key: NodeID, nav: char): NodeID =
+  if nav == 'L':
+    return map[key].left
+  elif nav == 'R':
+    return map[key].right
+
+
+proc walkMap(map: var NodeMap, startId: NodeID, nav: NavigationMap): NodeID =
+  let endID = map.get(startId, nav[0])
+
+  if nav.len > 1:
+    return walkMap(map, endID, nav[1..len(nav)-1])
+
+  return endID
 
 #
 # Main
@@ -30,8 +52,27 @@ if not fileExists(filePath):
 let appStartTime = cpuTime()
 echo "\n--- Loading Data File ---\n"
 let rawTextLines: seq[string] = readFileLines(filePath)
+let navMap = rawTextLines[0]
+var nodeMap = newTable[NodeID, NodePair]()
+
+for line in rawTextLines[2..(len(rawTextLines)-1)]:
+  var matches: array[3, string] 
+  discard match(line, re"(\w\w\w) = \((\w\w\w), (\w\w\w)\)", matches)
+  nodeMap[matches[0]] = (left: matches[1], right: matches[2])
+
+echo &"Node Map: {nodeMap}"
 
 echo "\n--- Part One ---\n"
+echo &"Nav Map: {navMap}"
+
+var optimizedNodeMap = newTable[NodeID, NodeID]()
+for key, val in nodeMap.pairs:
+  echo &"Key: {key}, Value: {val}"
+  echo walkMap(nodeMap, "AAA", navMap)
+
+#TODO: Load the raw map from file into a table[NodeID, NodePair]
+#      Then create an optimized table[NodeID, NodeID]
+#      Start at AAA, count the steps to ZZZ
 
 
 echo "\n--- Part Two ---\n"
